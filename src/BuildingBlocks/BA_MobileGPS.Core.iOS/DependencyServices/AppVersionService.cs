90900c3ff2d1827f7a03e645a57bdb2748e0ee43
linhlv <linhlv@bagroup.vn>
2020-07-06 16:50:19 +0700

    doing

---
+++ src/BuildingBlocks/BA_MobileGPS.Core.iOS/DependencyServices/AppVersionService.cs
@@ -0,0 +1,22 @@
+﻿using BA_MobileGPS.Core.iOS.DependencyServices;
+
+using Foundation;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AppVersionService))]
+
+namespace BA_MobileGPS.Core.iOS.DependencyServices
+{
+    public class AppVersionService : IAppVersionService
+    {
+        public string GetAppVersion()
+        {
+            return NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleShortVersionString").ToString();
+        }
+
+        public string GetAppBuild()
+
+        {
+            return NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleVersion").ToString();
+        }
+    }
+}
