598fc016ef73d36c3621c651a7ae98b167ab1b73
trần hoàng nam <namth2@binhanh.vn>
2020-07-08 09:21:30 +0700

    doing ble

--- src/BuildingBlocks/BA_MobileGPS.Core/Interfaces/HUDService.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/Interfaces/HUDService.cs
@@ -1,39 +1,37 @@
-﻿//using Acr.UserDialogs;
+﻿using System;
+using System.Threading.Tasks;
 
-//using System;
-//using System.Threading.Tasks;
+namespace BA_MobileGPS.Core
+{
+    public class HUDService : IDisposable
+    {
+        private bool _cancel;
 
-//namespace BA_MobileGPS.Core
-//{
-//    public class HUDService : IDisposable
-//    {
-//        private bool _cancel;
+        public HUDService(string message = "")
+        {
+            StartHUD(message);
+        }
 
-//        public HUDService(string message = "")
-//        {
-//            StartHUD(message);
-//        }
+        private async void StartHUD(string message)
+        {
+            await Task.Delay(100);
 
-//        private async void StartHUD(string message)
-//        {
-//            await Task.Delay(100);
+            if (_cancel)
+            {
+                _cancel = false;
+                return;
+            }
 
-//            if (_cancel)
-//            {
-//                _cancel = false;
-//                return;
-//            }
+            _cancel = false;
 
-//            _cancel = false;
+            //UserDialogs.Instance.ShowLoading(message, MaskType.Black);
+        }
 
-//            UserDialogs.Instance.ShowLoading(message, MaskType.Black);
-//        }
+        public void Dispose()
+        {
+            _cancel = true;
 
-//        public void Dispose()
-//        {
-//            _cancel = true;
-
-//            UserDialogs.Instance.HideLoading();
-//        }
-//    }
-//}
+            //UserDialogs.Instance.HideLoading();
+        }
+    }
+}
