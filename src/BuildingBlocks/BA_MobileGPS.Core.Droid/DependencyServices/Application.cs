2726e771926b2cada64372e73aa94c15d9cca659
trần hoàng nam <namth2@binhanh.vn>
2020-07-08 14:58:23 +0700

    fix keyborad

---
+++ src/BuildingBlocks/BA_MobileGPS.Core.Droid/DependencyServices/Application.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+[assembly: Dependency(typeof(BA_MobileGPS.Core.Droid.Application))]
+
+namespace BA_MobileGPS.Core.Droid
+{
+    public class Application : IApplication
+    {
+        public void SetWindowSoftInputMode(WindowSoftInputModeAdjust inputMode)
+        {
+            Xamarin.Forms.Application.Current.On<Xamarin.Forms.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust(inputMode);
+        }
+    }
+}
