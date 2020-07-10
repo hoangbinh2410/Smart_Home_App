a0049307199a83cf2baf36bc350a7bce6bb4aea7
linhlv <linhlv@bagroup.vn>
2020-07-10 13:20:48 +0700

    doing

---
+++ src/BuildingBlocks/BA_MobileGPS.Core/Events/ShowTabItemEvent.cs
@@ -0,0 +1,12 @@
+﻿using BA_MobileGPS.Entities;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BA_MobileGPS.Core
+{ 
+    public class ShowTabItemEvent : PubSubEvent<bool>
+    {
+    }
+}
