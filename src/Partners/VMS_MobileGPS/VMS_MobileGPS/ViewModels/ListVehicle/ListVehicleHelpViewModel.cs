ddc18e47ae0262ae6ea2f0b54b806d771b7b9d46
trần hoàng nam <namth2@binhanh.vn>
2020-07-13 16:00:01 +0700

    add listvehicle

---
+++ src/Partners/VMS_MobileGPS/VMS_MobileGPS/ViewModels/ListVehicle/ListVehicleHelpViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BA_MobileGPS.Core.ViewModels;
+
+using Prism.Navigation;
+
+namespace VMS_MobileGPS.ViewModels
+{
+    public class ListVehicleHelpViewModel : ViewModelBase
+    {
+        public ListVehicleHelpViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}
---
+++ src/Partners/VMS_MobileGPS/VMS_MobileGPS/Views/ListVehicle/VehicleExpiredView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Sharpnado.MaterialFrame;
+using Xamarin.Forms.Xaml;
+
+namespace VMS_MobileGPS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VehicleExpiredView : MaterialFrame
+    {
+        public VehicleExpiredView()
+        {
+            InitializeComponent();
+        }
+    }
+}
