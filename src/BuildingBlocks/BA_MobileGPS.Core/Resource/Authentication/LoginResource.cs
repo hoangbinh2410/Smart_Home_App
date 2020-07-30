508d1656e0e8a473dbadca580b975c0320bd4d68
trần hoàng nam <namth2@binhanh.vn>
2020-07-30 13:36:16 +0700

    doing

--- src/BuildingBlocks/BA_MobileGPS.Core/Resource/Authentication/LoginResource.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/Resource/Authentication/LoginResource.cs
@@ -32,7 +32,7 @@ namespace BA_MobileGPS.Core.Resource
         public static string Login_Popup_Manual => Get(MobileResourceNames.Login_Popup_Manual, "Hướng dẫn sử dụng", "User Manual");
         public static string Login_Popup_Guarantee => Get(MobileResourceNames.Login_Popup_Guarantee, "Thông tin bảo hành", "Guarantee Informaion");
         public static string Login_Popup_RegisterSupport => Get(MobileResourceNames.Login_Popup_RegisterSupport, "Đăng kí tư vấn", "Support Register");
-        public static string Login_Popup_BAGPSExperience => Get(MobileResourceNames.Login_Popup_BAGPSExperience, "Trải nghiệm BAGPS", "BAGPS Experience");
+        public static string Login_Popup_BAGPSExperience => Get(MobileResourceNames.Login_Popup_BAGPSExperience, "Trải nghiệm BA GPS", "BA GPS Experience");
 
 
     }
