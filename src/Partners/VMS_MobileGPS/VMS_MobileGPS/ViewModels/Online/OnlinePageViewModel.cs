ef074e4034302ca0a013b8a696f55fe97c60f23b
linhlv <linhlv@bagroup.vn>
2020-08-03 14:56:08 +0700

    doing

--- src/Partners/VMS_MobileGPS/VMS_MobileGPS/ViewModels/Online/OnlinePageViewModel.cs
+++ src/Partners/VMS_MobileGPS/VMS_MobileGPS/ViewModels/Online/OnlinePageViewModel.cs
@@ -68,7 +68,7 @@ namespace VMS_MobileGPS.ViewModels
 
             carActive = new VehicleOnline();
             selectedVehicleGroup = new List<int>();
-            CarSearch = MobileResource.Route_Label_SearchFishing;
+            CarSearch = string.Empty;
 
             if (MobileUserSettingHelper.MapType == 4 || MobileUserSettingHelper.MapType == 5)
             {
