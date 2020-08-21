e2b6c0331f0b0009da60916eb20b95fa18dd3333
ddoongvn <leehuyddoong@gmail.com>
2020-08-21 14:41:21 +0700

    .

--- src/BuildingBlocks/BA_MobileGPS.Core.iOS/DependencyServices/ScreenOrientServices.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core.iOS/DependencyServices/ScreenOrientServices.cs
@@ -12,12 +12,14 @@ namespace BA_MobileGPS.Core.iOS.DependencyServices
     {
         public void ForceLandscape()
         {           
-            UIDevice.CurrentDevice.SetValueForKey(new NSNumber((int)UIInterfaceOrientation.LandscapeLeft), new NSString("orientation"));         
+            UIDevice.CurrentDevice.SetValueForKey(new NSNumber((int)UIInterfaceOrientation.LandscapeLeft), new NSString("orientation"));
+            UIViewController.AttemptRotationToDeviceOrientation();
         }
 
         public void ForcePortrait()
         {          
             UIDevice.CurrentDevice.SetValueForKey(new NSNumber((int)UIInterfaceOrientation.Portrait), new NSString("orientation"));
+            UIViewController.AttemptRotationToDeviceOrientation();
         }
     }
 }
