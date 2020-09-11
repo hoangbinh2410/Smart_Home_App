edae2945ad5ba6358a635331893e9298d1fbdd22
trần hoàng nam <namth2@binhanh.vn>
2020-09-11 18:16:44 +0700

    done

--- src/BuildingBlocks/BA_MobileGPS.Utilities/Enums/MobileConfigurationNames.cs
+++ src/BuildingBlocks/BA_MobileGPS.Utilities/Enums/MobileConfigurationNames.cs
@@ -63,6 +63,7 @@
         NumberOfDigitVerifyCode,
         AuthenticationSecond,
         TimeVehicleSync,
+        TimmerVehicleSync,
         LengthAndPrefixNumberPhone,
         ConfigDangerousChar,
         ConfigUnitDebtMoney,
