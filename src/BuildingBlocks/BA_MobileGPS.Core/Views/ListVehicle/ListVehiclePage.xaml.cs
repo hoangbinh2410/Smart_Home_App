01568003507a3642740ba089ebb6969430b479f4
trần hoàng nam <namth2@binhanh.vn>
2020-10-30 09:55:42 +0700

    doing

--- src/BuildingBlocks/BA_MobileGPS.Core/Views/ListVehicle/ListVehiclePage.xaml.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/Views/ListVehicle/ListVehiclePage.xaml.cs
@@ -1,12 +1,5 @@
-﻿using BA_MobileGPS.Core.ViewModels;
-using BA_MobileGPS.Core.ViewModels.Base;
-using BA_MobileGPS.Utilities;
-using Prism;
-using Prism.Events;
-using Prism.Ioc;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using System;
-using System.Reflection;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,38 +10,17 @@ namespace BA_MobileGPS.Core.Views
     {
         private bool _loadedPage = false;
         private bool _stopTimer = false;
-        private ListVehiclePageViewModel vm;
-
-        private readonly IEventAggregator eventAggregator;
 
         public ListVehiclePage()
         {
-            try
-            {
-                InitializeComponent();
-
-                // Initialize the View Model Object
-                vm = (ListVehiclePageViewModel)BindingContext;
-            }
-            catch (Exception ex)
-            {
-                Logger.WriteError(MethodBase.GetCurrentMethod().Name, ex);
-            }
-
-            eventAggregator = PrismApplicationBase.Current.Container.Resolve<IEventAggregator>();
-            eventAggregator.GetEvent<DestroyEvent>().Subscribe(Destroy);
-        }
-
-        private void Destroy()
-        {
-            eventAggregator.GetEvent<DestroyEvent>().Unsubscribe(Destroy);
+            InitializeComponent();
         }
 
         protected override void OnAppearing()
         {
             if (_loadedPage) return;
 
-            Device.StartTimer(TimeSpan.FromMilliseconds(500), () =>
+            Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
             {
                 SetContent();
                 return false;
@@ -65,7 +37,8 @@ namespace BA_MobileGPS.Core.Views
             if (!_stopTimer)
             {
                 ViewModelLocator.SetAutowireViewModel(this, true);
-                Content = new ListVehicleView();
+                var view = new ListVehicleView();
+                Content = view;
 
                 _loadedPage = true;
             }
