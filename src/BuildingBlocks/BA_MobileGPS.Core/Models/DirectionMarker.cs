33e6eb2a1544faad63b9dadc01d2a67d66d53493
trần hoàng nam <namth2@binhanh.vn>
2020-11-06 10:00:05 +0700

    doing

---
+++ src/BuildingBlocks/BA_MobileGPS.Core/Models/DirectionMarker.cs
@@ -0,0 +1,46 @@
+﻿using Xamarin.Forms;
+
+namespace BA_MobileGPS.Core.Models
+{
+    public class DirectionMarker
+    {
+        /** Vị trí của marker */
+        public Position Position;
+
+        /** Hướng của marker */
+        public float Direction;
+
+
+        public string Label;
+
+        public DirectionMarker InitDirectionPoint(double fromLat, double fromLng, double toLat, double toLng, string label)
+        {
+            // Tính hướng
+            Direction = (float)GeoHelper.ComputeHeading(fromLat, fromLng, toLat, toLng);
+            // Vị trí marker hướng
+            Position = new Position((fromLat + toLat) / 2, (fromLng + toLng) / 2);
+
+            Label = label;
+            return this;
+        }
+
+        /* Vẽ điểm marker hướng */
+
+        public Pin DrawPointDiretion()
+        {
+            var pin = new Pin()
+            {
+                Type = PinType.Place,
+                Label = Label,
+                Anchor = new Point(.5, .5),
+                Position = Position,
+                Rotation = Direction,
+                Icon = BitmapDescriptorFactory.FromResource("ic_arrow_tracking.png"),
+                Tag = "direction",
+                ZIndex = 1,
+                IsDraggable = false
+            };
+            return pin;
+        }
+    }
+}
