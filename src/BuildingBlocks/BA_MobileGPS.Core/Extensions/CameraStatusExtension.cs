9565eb00f92292fa5fd750f0c673d631ffd12cb0
ddoongvn <leehuyddoong@gmail.com>
2020-11-20 11:58:28 +0700

    .

---
+++ src/BuildingBlocks/BA_MobileGPS.Core/Extensions/CameraStatusExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BA_MobileGPS.Core.Extensions
+{
+    public static class CameraStatusExtension
+    {
+        public static bool IsLiveStreaming(int state)
+        {
+            return true;
+        }
+
+        public static bool IsRestreaming(int state)
+        {
+            if (state == 2 || state == 3 || state == 6)
+            {
+                return true;
+            }
+            else return false;
+        }
+        //private int[] ConvertToBit(int state)
+        //{
+        //    BitArray b = new BitArray(new byte[] { state });
+        //    int[] bits = b.Cast<bool>().Select(bit => bit ? 1 : 0).ToArray();
+        //}
+    }
+}
---
+++ src/BuildingBlocks/BA_MobileGPS.Core/Models/ChannelModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BA_MobileGPS.Core.Models
+{
+    public class ChannelModel
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
