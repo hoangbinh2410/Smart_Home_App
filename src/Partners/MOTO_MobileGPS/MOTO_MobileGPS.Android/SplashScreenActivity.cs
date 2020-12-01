6c9c2ae19bf490d6d1e995297654fa2d9bf52999
trần hoàng nam <namth2@binhanh.vn>
2020-12-01 10:06:11 +0700

    fix BA MOTO

--- src/Partners/MOTO_MobileGPS/MOTO_MobileGPS.Android/SplashScreenActivity.cs
+++ src/Partners/MOTO_MobileGPS/MOTO_MobileGPS.Android/SplashScreenActivity.cs
@@ -1,31 +1,18 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.OS;
 using Android.Support.V7.App;
 
 namespace MOTO_MobileGPS.Droid
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true, NoHistory = true, Theme = "@style/Theme.Splash", ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "@string/app_name", MainLauncher = true, NoHistory = true, Theme = "@style/MainTheme.Splash", ScreenOrientation = ScreenOrientation.Portrait)]
     public class SplashScreenActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        // Launches the startup task
+        protected override void OnResume()
         {
-            base.OnCreate(savedInstanceState);
-
-            var intent = new Intent(this, typeof(MainActivity));
-
-            // Create your application here
-            var mainIntent = new Intent(Application.Context, typeof(MainActivity));
-
-            if (Intent.Extras != null)
-            {
-                mainIntent.PutExtras(Intent.Extras);
-            }
-            //mainIntent.SetFlags(ActivityFlags.SingleTop);
-
-            StartActivity(intent);
-            Finish();
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
     }
 }
