6a5b07b350f64ea0c57285ed95c040cb41eb8c8f
trần hoàng nam <namth2@binhanh.vn>
2021-01-19 09:05:03 +0700

    doing

---
+++ src/BuildingBlocks/BA_MobileGPS.Core.iOS/CustomRenderer/MyDatePickerRenderer.cs
@@ -0,0 +1,34 @@
+﻿using BA_MobileGPS.Core.iOS.CustomRenderer;
+using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(MyDatePickerRenderer))]
+
+namespace BA_MobileGPS.Core.iOS.CustomRenderer
+{
+    public class MyDatePickerRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && this.Control != null)
+            {
+                try
+                {
+                    if (UIDevice.CurrentDevice.CheckSystemVersion(13, 2))
+                    {
+                        UIDatePicker picker = (UIDatePicker)Control.InputView;
+                        picker.PreferredDatePickerStyle = UIDatePickerStyle.Wheels;
+                    }
+                }
+                catch (Exception)
+                {
+                    // do nothing
+                }
+            }
+        }
+    }
+}
