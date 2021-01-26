438f5cb7cd1d7539d51ad37b3cc3216a1869ba45
trần hoàng nam <namth2@binhanh.vn>
2021-01-26 14:01:33 +0700

    .

--- src/Partners/VMS_MobileGPS/VMS_MobileGPS/Views/Online/OnlinePage.xaml.cs
+++ src/Partners/VMS_MobileGPS/VMS_MobileGPS/Views/Online/OnlinePage.xaml.cs
@@ -227,11 +227,11 @@ namespace VMS_MobileGPS.Views
                 }
                 else if (boundary.PK_LandmarkID == 376651)
                 {
-                    color = Color.FromRgba(0, 0, 255, 0.5);
+                    color = Color.Blue;
                 }
                 else if (boundary.PK_LandmarkID == 376652)
                 {
-                    color = Color.FromRgba(0, 128, 0, 0.5);
+                    color = Color.Green;
                 }
 
                 if (boundary.IsClosed)
@@ -240,8 +240,8 @@ namespace VMS_MobileGPS.Views
                     {
                         IsClickable = true,
                         StrokeWidth = 1f,
-                        StrokeColor = color,
-                        FillColor = color,
+                        StrokeColor = color.MultiplyAlpha(.5),
+                        FillColor = color.MultiplyAlpha(.3),
                         Tag = "POLYGON"
                     };
 
@@ -258,7 +258,7 @@ namespace VMS_MobileGPS.Views
                     var polyline = new Polyline
                     {
                         IsClickable = false,
-                        StrokeColor = color.MultiplyAlpha(.3),
+                        StrokeColor = color,
                         StrokeWidth = 2f,
                         Tag = "POLYGON"
                     };
