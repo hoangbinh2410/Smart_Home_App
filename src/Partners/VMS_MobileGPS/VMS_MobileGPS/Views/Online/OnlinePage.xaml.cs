3298d16fac4f2e50f01df5c300fa953831522677
trần hoàng nam <namth2@binhanh.vn>
2021-01-26 13:33:03 +0700

    .

--- src/Partners/VMS_MobileGPS/VMS_MobileGPS/Views/Online/OnlinePage.xaml.cs
+++ src/Partners/VMS_MobileGPS/VMS_MobileGPS/Views/Online/OnlinePage.xaml.cs
@@ -227,11 +227,11 @@ namespace VMS_MobileGPS.Views
                 }
                 else if (boundary.PK_LandmarkID == 376651)
                 {
-                    color = Color.Blue;
+                    color = Color.FromRgba(0, 0, 255, 0.5);
                 }
                 else if (boundary.PK_LandmarkID == 376652)
                 {
-                    color = Color.Green;
+                    color = Color.FromRgba(0, 128, 0, 0.5);
                 }
 
                 if (boundary.IsClosed)
