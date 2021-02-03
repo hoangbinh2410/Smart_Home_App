e2ca222ce36e97838a30d56018d41f8b322644d2
ddoongvn <39513425+ddoongvn@users.noreply.github.com>
2021-02-03 13:15:54 +0700

    .

---
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/PapersInformation/ListPaperHistories/PaperHistoriesPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BA_MobileGPS.Core.ViewModels
+{
+    public class PaperHistoriesPageViewModel : ViewModelBase
+    {
+        public PaperHistoriesPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+
+        }
+    }
+}
---
+++ src/BuildingBlocks/BA_MobileGPS.Entities/ResponeEntity/PaperItemHistoryModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BA_MobileGPS.Entities.ResponeEntity
+{
+    /// <summary>
+    /// Thong tin item trong list lịch sử giấy tờ
+    /// Các mục comment là dữ liệu không dùng ở UI
+    /// </summary>
+    public class PaperItemHistoryModel
+    {
+        public Guid Id { get; set; }
+        //public int FKCompanyID { get; set; }
+        public int FK_VehicleID { get; set; }
+        public Guid FK_PaperCategoryID { get; set; }
+        public string PaperCategoryName { get; set; }
+        //public string PaperNumber { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public DateTime ExpireDate { get; set; }
+        //public int DayOfAlertBefore { get; set; }
+        //public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        //public Guid CreatedByUser { get; set; }
+        public string UserCreated { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        //public Guid UpdatedByUser { get; set; }
+        public string UserUpdated { get; set; }
+    }
+
+    public class PaperHistoriesResponse : ResponseBaseV2<DataResponseBase<PaperItemHistoryModel>>
+    {
+
+    }
+}
