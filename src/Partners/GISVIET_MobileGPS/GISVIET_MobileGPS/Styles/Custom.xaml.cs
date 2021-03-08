bd2414b649a6d250781304096e652545860efd99
ddoongvn <39513425+ddoongvn@users.noreply.github.com>
2021-03-08 15:00:33 +0700

    init

---
+++ src/Partners/GISVIET_MobileGPS/GISVIET_MobileGPS/Styles/Custom.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GISVIET_MobileGPS.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Custom : ResourceDictionary
+    {
+        public Custom()
+        {
+            InitializeComponent();
+        }
+    }
+}
