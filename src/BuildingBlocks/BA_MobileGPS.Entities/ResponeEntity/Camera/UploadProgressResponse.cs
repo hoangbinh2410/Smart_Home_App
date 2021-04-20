ce494b8e3fced2eb43f79d80d11b5271028a774d
trần hoàng nam <namth2@binhanh.vn>
2021-04-20 14:15:09 +0700

    doing

---
+++ src/BuildingBlocks/BA_MobileGPS.Entities/ResponeEntity/Camera/UploadProgressResponse.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BA_MobileGPS.Entities.ResponeEntity.Camera
+{
+    public class UploadProgressResponse
+    {
+        // Convert từ PNC dùng CustomerID
+        [JsonProperty("CustomerID")]
+        public int XnCode { get; set; }
+
+        // Convert từ PNC dùng VehicleName
+        [JsonProperty("VehicleName")]
+        public string VehiclePlate { get; set; }
+
+        public byte Channel { get; set; }
+
+        //Tên file đang thực hiện upload
+        public string CurrentFile { get; set; }
+
+        //Tổng số file upload không thành công
+        public byte ErrorCount { get; set; }
+
+        //Tổng số file đã upload thành công
+        public byte FinishCount { get; set; }
+
+        //Tổng số file trong phiên upload
+        public byte TotalCount { get; set; }
+
+        //Thời điểm cập nhật dữ liệu tiến trình
+        public DateTime UpdatedTime { get; set; }
+    }
+}
