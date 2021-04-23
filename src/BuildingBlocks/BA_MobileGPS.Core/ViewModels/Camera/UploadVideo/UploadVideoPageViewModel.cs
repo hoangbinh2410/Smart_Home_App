65ae27dc87803f16379c25797f5c592902fcdd9b
trần hoàng nam <namth2@binhanh.vn>
2021-04-23 13:14:53 +0700

    doing

--- src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/Camera/UploadVideo/UploadVideoPageViewModel.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/Camera/UploadVideo/UploadVideoPageViewModel.cs
@@ -1,16 +1,59 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using BA_MobileGPS.Entities;
+using BA_MobileGPS.Service.IService;
+using Prism.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
+using Xamarin.Forms.Extensions;
 
 namespace BA_MobileGPS.Core.ViewModels
 {
-    public class UploadVideoPageViewModel : BindableBase
+    public class UploadVideoPageViewModel : ViewModelBase
     {
-        public UploadVideoPageViewModel()
-        {
+        private readonly IStreamCameraService streamCameraService;
 
+        public UploadVideoPageViewModel(INavigationService navigationService, IStreamCameraService streamCameraService) : base(navigationService)
+        {
+            this.streamCameraService = streamCameraService;
+        }
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            base.Initialize(parameters);
+
+            GetListVideoUploadActive();
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        private ObservableCollection<CameraRestreamInfo> listVideo = new ObservableCollection<CameraRestreamInfo>();
+        public ObservableCollection<CameraRestreamInfo> ListVideo { get => listVideo; set => SetProperty(ref listVideo, value); }
+
+        private void GetListVideoUploadActive()
+        {
+            var req = new CameraRestreamRequest()
+            {
+                CustomerId = 1010,
+                Date = Convert.ToDateTime("2021-04-14"),
+                VehicleNames = "CAMTEST2"
+            };
+            RunOnBackground(async () =>
+            {
+                return await streamCameraService.GetListVideoOnDevice(req);
+            }, (result) =>
+            {
+                if (result != null && result.Count > 0)
+                {
+                    ListVideo = result.ToObservableCollection();
+                }
+            });
         }
     }
 }
--- src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/MainPage/AccountTab/AccountViewModel.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/MainPage/AccountTab/AccountViewModel.cs
@@ -83,7 +83,7 @@ namespace BA_MobileGPS.Core.ViewModels
                 Title = "Phản hồi thông tin khách hàng",
                 Icon = "ic_customersupport.png",
                 UseModalNavigation = true,
-                Url = "NavigationPage/ListIssuePage",
+                Url = "BaseNavigationPage/UploadVideoPage",
                 MenuType = MenuType.DeviceManual,
                 IsEnable = true,
                 IconColor = Color.FromHex("#FF9900")
