0902616181d1bcd82e994dbbbf2b674d29ae3b41
trần hoàng nam <namth2@binhanh.vn>
2021-04-26 10:12:30 +0700

    fix thông báo ba cam

--- src/BuildingBlocks/BA_MobileGPS.Core/Models/CameraManagement.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/Models/CameraManagement.cs
@@ -320,6 +320,11 @@ namespace BA_MobileGPS.Core.Models
                         Link = string.Empty
                     };
                     _eventAggregator.GetEvent<SendErrorCameraEvent>().Publish(Channel);
+
+                    if (requestStartResponse.StatusCode == StatusCodeCamera.ERROR_STREAMING_BY_PLAYBACK)
+                    {
+                        requestStartResponse.UserMessage = "Thiết bị đang ở chế độ xem lại, quý khách vui lòng tắt xem lại để xem trực tiếp";
+                    }
                     SetError(requestStartResponse.UserMessage);
                 }
             });
