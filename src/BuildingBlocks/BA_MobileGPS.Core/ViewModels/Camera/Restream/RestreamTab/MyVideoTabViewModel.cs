cc6fb89a54d6a6e317370ed47fd509d09bd94950
trần hoàng nam <namth2@binhanh.vn>
2021-05-06 14:15:25 +0700

    doing

--- src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/Camera/Restream/RestreamTab/MyVideoTabViewModel.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/Camera/Restream/RestreamTab/MyVideoTabViewModel.cs
@@ -3,21 +3,40 @@ using BA_MobileGPS.Core.Interfaces;
 using BA_MobileGPS.Entities;
 using BA_MobileGPS.Service.IService;
 using LibVLCSharp.Shared;
+using Plugin.Permissions;
+using Prism.Commands;
 using Prism.Navigation;
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
+using System.Windows.Input;
+using Xamarin.Forms;
 using Xamarin.Forms.Extensions;
 
 namespace BA_MobileGPS.Core.ViewModels
 {
     public class MyVideoTabViewModel : RestreamChildVMBase
     {
+        public ICommand ScreenShotTappedCommand { get; }
+
+        /// <summary>
+        /// Chụp ảnh màn hình
+        /// </summary>
+        public ICommand DowloadVideoCommand { get; }
+
+        private readonly IDownloadVideoService _downloadService;
+
         public MyVideoTabViewModel(INavigationService navigationService,
           IStreamCameraService cameraService,
-          IScreenOrientServices screenOrientServices) : base(navigationService, cameraService, screenOrientServices)
+          IScreenOrientServices screenOrientServices, IDownloadVideoService downloadService) : base(navigationService, cameraService, screenOrientServices)
         {
+            _downloadService = downloadService;
+            VideoItemTapCommand = new DelegateCommand<VideoUploadInfo>(VideoSelectedChange);
+            ScreenShotTappedCommand = new DelegateCommand(TakeSnapShot);
+            DowloadVideoCommand = new DelegateCommand(DowloadVideo);
         }
 
         #region Lifecycle
@@ -36,6 +55,8 @@ namespace BA_MobileGPS.Core.ViewModels
         public override void OnPageAppearingFirstTime()
         {
             base.OnPageAppearingFirstTime();
+            LibVLCSharp.Shared.Core.Initialize();
+            LibVLC = new LibVLC();
             InitVLC();
         }
 
@@ -85,6 +106,36 @@ namespace BA_MobileGPS.Core.ViewModels
             get => mediaPlayer; set => SetProperty(ref mediaPlayer, value);
         }
 
+        private VideoUploadInfo videoSlected;
+
+        /// <summary>
+        /// Ảnh được focus
+        /// </summary>
+        public VideoUploadInfo VideoSlected
+        {
+            get => videoSlected;
+            set
+            {
+                SetProperty(ref videoSlected, value);
+            }
+        }
+
+        private double _progressValue;
+
+        public double ProgressValue
+        {
+            get { return _progressValue; }
+            set { SetProperty(ref _progressValue, value); }
+        }
+
+        private bool _isDownloading;
+
+        public bool IsDownloading
+        {
+            get { return _isDownloading; }
+            set { SetProperty(ref _isDownloading, value); }
+        }
+
         #endregion Property
 
         #region PrivateMethod
@@ -124,15 +175,10 @@ namespace BA_MobileGPS.Core.ViewModels
         {
             try
             {
-                LibVLCSharp.Shared.Core.Initialize();
-
-                LibVLC = new LibVLC(enableDebugLogs: true);
-                var media = new Media(LibVLC, new Uri("https://video35.binhanh.vn/2021/05/05/Video1010/05052021_150403_CAM1.mp4"));
-                MediaPlayer = new MediaPlayer(media) { EnableHardwareDecoding = true };
+                MediaPlayer = new MediaPlayer(LibVLC) { EnableHardwareDecoding = true };
                 MediaPlayer.TimeChanged += Media_TimeChanged;
                 MediaPlayer.EndReached += Media_EndReached;
                 MediaPlayer.EncounteredError += Media_EncounteredError;
-                media.Dispose();
                 MediaPlayer.Play();
             }
             catch (Exception ex)
@@ -190,6 +236,109 @@ namespace BA_MobileGPS.Core.ViewModels
 
         private void Media_TimeChanged(object sender, MediaPlayerTimeChangedEventArgs e)
         {
+            if (MediaPlayer.Time > 1 && BusyIndicatorActive)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    BusyIndicatorActive = false;
+                });
+            }
+        }
+
+        /// <summary>
+        /// Đóng video : Đóng khi current tab thay đổi
+        /// Chức năng trên player : không làm (đã confirm)
+        /// </summary>
+        private void CloseVideo()
+        {
+            if (MediaPlayerVisible)
+            {
+                MediaPlayerVisible = false;
+            }
+            //can remove media plaer de tranh loi man hinh den
+            DisposeVLC();
+        }
+
+        private void VideoSelectedChange(VideoUploadInfo obj)
+        {
+            if (obj != null)
+            {
+                SafeExecute(() =>
+                {
+                    if (MediaPlayerVisible)
+                    {
+                        CloseVideo();
+                    }
+
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        IsError = false;
+                        BusyIndicatorActive = true;
+                        MediaPlayerVisible = true; // Bật layout media lên
+                    });
+                    // init ở đây :
+                    InitVLC();
+                    MediaPlayer.Media = new Media(libVLC, new Uri(obj.Link));
+                    MediaPlayer.Play();
+                    VideoSlected = obj; // Set màu select cho item
+                });
+            }
+        }
+
+        private void TakeSnapShot()
+        {
+            try
+            {
+                var folderPath = DependencyService.Get<ICameraSnapShotServices>().GetFolderPath();
+                var current = DateTime.Now.ToString("yyyyMMddHHmmss");
+                var fileName = current + ".jpg";
+                var filePath = Path.Combine(folderPath, fileName);
+
+                if (VideoSlected != null)
+                {
+                    MediaPlayer.TakeSnapshot(0, filePath, 0, 0);
+                }
+                if (File.Exists(filePath))
+                {
+                    DependencyService.Get<ICameraSnapShotServices>().SaveSnapShotToGalery(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.WriteError(MethodBase.GetCurrentMethod().Name, ex);
+            }
+        }
+
+        private void DowloadVideo()
+        {
+            SafeExecute(async () =>
+            {
+                if (VideoSlected != null && !string.IsNullOrEmpty(VideoSlected.Link))
+                {
+                    var action = await PageDialog.DisplayAlertAsync("Thông báo", "Bạn có muốn tải video này về điện thoại không ?", "Đồng ý", "Bỏ qua");
+                    if (action)
+                    {
+                        var progressIndicator = new Progress<double>(ReportProgress);
+                        var cts = new CancellationTokenSource();
+                        IsDownloading = true;
+                        var permissionStatus = await CrossPermissions.Current.RequestPermissionAsync<StoragePermission>();
+                        if (permissionStatus == Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+                        {
+                            await _downloadService.DownloadFileAsync(VideoSlected.Link, progressIndicator, cts.Token);
+                        }
+                    }
+                }
+            });
+        }
+
+        internal void ReportProgress(double value)
+        {
+            ProgressValue = value;
+            if (value == 100)
+            {
+                DisplayMessage.ShowMessageInfo("Đã tải video thành công");
+                IsDownloading = false;
+            }
         }
 
         #endregion PrivateMethod
