887e73777d6e54e0829c9bca292e1974c93c15ae
trần hoàng nam <namth2@binhanh.vn>
2021-06-29 17:29:08 +0700

    .

--- src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/MainPage/MainPageViewModel.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/MainPage/MainPageViewModel.cs
@@ -1111,7 +1111,7 @@ namespace BA_MobileGPS.Core.ViewModels
 
                    Device.BeginInvokeOnMainThread(() =>
                    {
-                       DisplayMessage.ShowMessageInfo("Đã tải xong 1 video. Xem trên tab Video đã tải");
+                       DisplayMessage.ShowMessageInfo("Đã tải xong 1 video. Vui lòng xem trên tab Video đã tải");
                    });
                }
                else
