282ff3e47a78ef747369af0b92ff0798b82bf6ac
trần hoàng nam <namth2@binhanh.vn>
2021-07-26 14:50:01 +0700

    .

--- src/Partners/UNITEL_MobileGPS/UNITEL_MobileGPS/UNITELApp.cs
+++ src/Partners/UNITEL_MobileGPS/UNITEL_MobileGPS/UNITELApp.cs
@@ -22,18 +22,18 @@ namespace UNITEL_MobileGPS
         protected async override void OnInitialized()
         {
             base.OnInitialized();
-            ServerConfig.ServerIdentityHubType = ServerIdentityHubTypes.ServerThat;
-            ServerConfig.ServerVehicleOnlineHubType = ServerVehicleOnlineHubTypes.ServerThat;
-            ServerConfig.ServerAlertHubType = ServerAlertHubTypes.ServerThat;
-            ServerConfig.ServerUserBehaviorHubType = ServerUserBehaviorHubTypes.ServerThat;
-            ServerConfig.ApiEndpointTypes = ApiEndpointTypes.ServerThat;
+            ServerConfig.ServerIdentityHubType = ServerIdentityHubTypes.ServerGISVIET;
+            ServerConfig.ServerVehicleOnlineHubType = ServerVehicleOnlineHubTypes.ServerGISVIET;
+            ServerConfig.ServerAlertHubType = ServerAlertHubTypes.ServerGISVIET;
+            ServerConfig.ServerUserBehaviorHubType = ServerUserBehaviorHubTypes.ServerGISVIET;
+            ServerConfig.ApiEndpointTypes = ApiEndpointTypes.ServerGISVIET;
 
             Application.Current.Resources.MergedDictionaries.Add(new LightColor());
             Application.Current.Resources.MergedDictionaries.Add(new BA_MobileGPS.Core.Styles.Styles());
 
-            AppCenter.Start("ios=b9feff6c-5277-4e97-97e9-8a8e5c939eef;" +
-                   "android=db0089bc-c6e2-4df4-bead-0368ccef3cd6",
-                   typeof(Analytics), typeof(Crashes));
+            //AppCenter.Start("ios=b9feff6c-5277-4e97-97e9-8a8e5c939eef;" +
+            //       "android=db0089bc-c6e2-4df4-bead-0368ccef3cd6",
+            //       typeof(Analytics), typeof(Crashes));
 
             //Nếu cài app lần đầu tiên hoặc có sự thay đổi dữ liệu trên server thì sẽ vào trang cập nhật thông tin vào localDB
             if (!Settings.IsFistInstallApp || Settings.IsChangeDataLocalDB)
@@ -49,7 +49,7 @@ namespace UNITEL_MobileGPS
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             base.RegisterTypes(containerRegistry);
-            AppType = BA_MobileGPS.Entities.AppType.BinhAnh;
+            AppType = BA_MobileGPS.Entities.AppType.GisViet;
         }
     }
 }
