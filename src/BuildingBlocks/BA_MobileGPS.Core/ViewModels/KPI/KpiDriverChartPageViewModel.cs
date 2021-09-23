6257bca318a77d4ad5ab61bedb053994c92fa136
trần hoàng nam <namth2@binhanh.vn>
2021-09-23 17:53:23 +0700

    doing

--- src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/KPI/KpiDriverChartPageViewModel.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/KPI/KpiDriverChartPageViewModel.cs
@@ -47,12 +47,22 @@ namespace BA_MobileGPS.Core.ViewModels
         private ChartSeriesCollection chartSeries = new ChartSeriesCollection();
         public ChartSeriesCollection ChartSeries { get => chartSeries; set => SetProperty(ref chartSeries, value); }
 
+        private ChartSeriesCollection chartSeries2 = new ChartSeriesCollection();
+        public ChartSeriesCollection ChartSeries2 { get => chartSeries2; set => SetProperty(ref chartSeries2, value); }
+
         public ObservableCollection<ChartDataModel> ChartDataSource { get; set; }
 
         public ObservableCollection<ChartDataModel> ChartDataSourceActual { get; set; }
 
-        private EvaluationsByType evaluationType1=new EvaluationsByType();
+        public ObservableCollection<ChartDataModel> ChartDataSource2 { get; set; }
+
+        public ObservableCollection<ChartDataModel> ChartDataSourceActual2 { get; set; }
+
+        private EvaluationsByType evaluationType1 = new EvaluationsByType();
         public EvaluationsByType EvaluationType1 { get => evaluationType1; set => SetProperty(ref evaluationType1, value); }
+
+        private EvaluationsByType evaluationType2 = new EvaluationsByType();
+        public EvaluationsByType EvaluationType2 { get => evaluationType2; set => SetProperty(ref evaluationType2, value); }
 
         #endregion Property
 
@@ -71,6 +81,7 @@ namespace BA_MobileGPS.Core.ViewModels
                 if (result != null && result.EvaluationsByType != null && result.EvaluationsByType.Count > 0)
                 {
                     GenChartEvaluationType1(result.EvaluationsByType);
+                    GenChartEvaluationType2(result.EvaluationsByType);
                 }
             }, showLoading: true);
         }
@@ -96,6 +107,31 @@ namespace BA_MobileGPS.Core.ViewModels
                         }
                     }
                     DrawChartSeries();
+                }
+            }
+        }
+
+        private void GenChartEvaluationType2(List<EvaluationsByType> list)
+        {
+            if (list != null && list.Count > 0)
+            {
+                ChartDataSource2 = new ObservableCollection<ChartDataModel>();
+                ChartDataSourceActual2 = new ObservableCollection<ChartDataModel>();
+                var ItemEvaluation = list.FirstOrDefault(x => x.TypeId == 2);
+                if (ItemEvaluation != null)
+                {
+                    EvaluationType2 = ItemEvaluation;
+                    foreach (var item in ItemEvaluation.Evaluations)
+                    {
+                        var rank = GetTilteRank(item.OrderDisPlay);
+                        ChartDataSource2.Add(new ChartDataModel(item.Name, rank.Value));
+                        if (item.OrderDisPlayActual != null)
+                        {
+                            var rankactual = GetTilteRank(item.OrderDisPlayActual.GetValueOrDefault());
+                            ChartDataSourceActual2.Add(new ChartDataModel(item.Name, rankactual.Value));
+                        }
+                    }
+                    DrawChartSeries2();
                 }
             }
         }
@@ -130,6 +166,38 @@ namespace BA_MobileGPS.Core.ViewModels
             chart.Add(series2);
 
             ChartSeries = chart;
+        }
+
+        private void DrawChartSeries2()
+        {
+            ChartSeries2.Clear();
+            var chart = new ChartSeriesCollection();
+            var series = new RadarSeries()
+            {
+                Label = "Điểm tiêu chuẩn",
+                Opacity = .6,
+                StrokeWidth = 2,
+                DrawType = PolarRadarSeriesDrawType.Line,
+                LegendIcon = ChartLegendIcon.Rectangle,
+                XBindingPath = nameof(ChartDataModel.Name),
+                YBindingPath = nameof(ChartDataModel.Value),
+                ItemsSource = ChartDataSource2,
+            };
+            var series2 = new RadarSeries()
+            {
+                Label = "Điểm thực tế",
+                Opacity = .6,
+                StrokeWidth = 2,
+                DrawType = PolarRadarSeriesDrawType.Line,
+                LegendIcon = ChartLegendIcon.Rectangle,
+                XBindingPath = nameof(ChartDataModel.Name),
+                YBindingPath = nameof(ChartDataModel.Value),
+                ItemsSource = ChartDataSourceActual2,
+            };
+            chart.Add(series);
+            chart.Add(series2);
+
+            ChartSeries2 = chart;
         }
 
         private KPIRank GetTilteRank(int orderDisPlay)
