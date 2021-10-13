f36afe95524c153f2313ea1268741bd17eb26c88
Hoàng Bình <binhht2@bagroup.vn>
2021-10-13 17:49:54 +0700

    View

---
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/Support/NotificationSupportPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BA_MobileGPS.Core.Resources;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BA_MobileGPS.Core.ViewModels
+{
+    public class NotificationSupportPageViewModel : ViewModelBase
+    {
+        public NotificationSupportPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}
---
+++ src/BuildingBlocks/BA_MobileGPS.Core/ViewModels/Support/SupportFeePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BA_MobileGPS.Core.Resources;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BA_MobileGPS.Core.ViewModels
+{
+    public class SupportFeePageViewModel : ViewModelBase
+    {
+        public SupportFeePageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+           
+        }
+    }
+}
