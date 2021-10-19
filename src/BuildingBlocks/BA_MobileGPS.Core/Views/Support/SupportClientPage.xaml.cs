3a72d6daabbec4eb0244cea1c91f58fcfa442db0
Hoàng Bình <binhht2@bagroup.vn>
2021-10-19 09:33:05 +0700

    update.

--- src/BuildingBlocks/BA_MobileGPS.Core/Views/Support/SupportClientPage.xaml.cs
+++ src/BuildingBlocks/BA_MobileGPS.Core/Views/Support/SupportClientPage.xaml.cs
@@ -10,10 +10,10 @@ namespace BA_MobileGPS.Core.Views
         public SupportClientPage()
         {
             InitializeComponent();
-            //this.title.Text = MobileResource.SupportClient_Label_Title;
-            //this.vehicleProcessing.Text = MobileResource.SupportClient_Label_VehicleProcessing;
-            //this.supportCategory.Text = MobileResource.SupportClient_Label_SupportCategory;
-            //this.textSupport.Text = MobileResource.SupportClient_Label_TextSupport;
+            this.title.Text = MobileResource.SupportClient_Label_Title;
+            this.vehicleProcessing.Text = MobileResource.SupportClient_Label_VehicleProcessing;
+            this.supportCategory.Text = MobileResource.SupportClient_Label_SupportCategory;
+            this.textSupport.Text = MobileResource.SupportClient_Label_TextSupport;
             //this.lostSignal.Text = MobileResource.SupportClient_Label_LostSignal;
             //this.changeNumberPlate.Text = MobileResource.SupportClient_Label_ChangeNumberPlate;
             //this.cameraError.Text = MobileResource.SupportClient_Label_CameraError;
