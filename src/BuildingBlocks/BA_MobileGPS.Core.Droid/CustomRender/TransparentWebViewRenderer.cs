addbe0c7b5bcc61035b8d9b06fe1683d37705762
trần hoàng nam <namth2@binhanh.vn>
2021-11-12 09:33:32 +0700

    doing

---
+++ src/BuildingBlocks/BA_MobileGPS.Core.Droid/CustomRender/TransparentWebViewRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using BA_MobileGPS.Core.Droid.CustomRender;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRendererAttribute(typeof(WebView), typeof(TransparentWebViewRenderer))]
+
+namespace BA_MobileGPS.Core.Droid.CustomRender
+{
+    public class TransparentWebViewRenderer : WebViewRenderer
+    {
+        public TransparentWebViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            // Setting the background as transparent
+            this.Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
